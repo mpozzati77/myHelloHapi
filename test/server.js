1b811b2e2d29b6177954796b0630509c07561ce6
riccardoscalet <rscalet@techimp.com>
2016-09-28 15:04:13 +0200

    Added test files.

--- test/server.js
+++ test/server.js
@@ -6,21 +6,21 @@ const lab = exports.lab = Lab.script({ output: process.stdout })
 const build = require('../')
 
 lab.experiment('Hello', () => {
-  let server
+    let server
 
-  lab.beforeEach((done) => {
-    build({ port: 8989 }, (err, s) => {
-      server = s
-      done(err)
+    lab.beforeEach((done) => {
+        build({ port: 8989 }, (err, s) => {
+            server = s
+            done(err)
+        })
     })
-  })
 
-  lab.test('Testing for "Hello World"', (done) => {
-    const options = { method: 'GET', url: '/' }
-    server.inject(options, function (response) {
-      const result = response.result
-      code.expect(result).to.equal('Hello World')
-      done()
+    lab.test('Testing for "Hello World"', (done) => {
+        const options = { method: 'GET', url: '/hello' }
+        server.inject(options, function(response) {
+            const result = response.result
+            code.expect(result).to.equal('Hello World')
+            done()
+        })
     })
-  })
 })
---
+++ testTap/assetService_test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const test = require('tap');
+
+test('add - ', function(t) {
+    //Arrange
+ 
+    //Act
+});
