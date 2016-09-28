b47c0a94989c7761ba93f4f7e985bf69f7389844
riccardoscalet <rscalet@techimp.com>
2016-09-28 12:05:44 +0200

    Added assetplugin.

--- hello.js
+++ hello.js
@@ -6,44 +6,50 @@ const Hapi = require('hapi')
 const xtend = require('xtend')
 const minimist = require('minimist')
 const defaults = {
-  port: 8989
+    port: 8989
 }
 
-function build (opts, cb) {
-  opts = xtend(defaults, opts)
+function build(opts, cb) {
+    opts = xtend(defaults, opts)
 
-  const server = new Hapi.Server()
+    const server = new Hapi.Server()
 
-  server.connection({ port: opts.port })
+    server.connection({ port: opts.port })
 
-  server.register([
-    require('./lib/myplugin')
-  ], (err) => {
-    cb(err, server)
-  })
-
-  return server
-}
-
-function start (opts) {
-  build(opts, (err, server) => {
-    if (err) { throw err }
-
-    server.start(function (err) {
-      if (err) { throw err }
-
-      console.log('Server running at:', server.info.uri)
+    server.register([
+        require('./lib/myplugin')
+    ], (err) => {
+        cb(err, server)
     })
-  })
+
+    server.register([
+        require('./lib/assetplugin')
+    ], (err) => {
+        cb(err, server)
+    })
+
+    return server
+}
+
+function start(opts) {
+    build(opts, (err, server) => {
+        if (err) { throw err }
+
+        server.start(function(err) {
+            if (err) { throw err }
+
+            console.log('Server running at:', server.info.uri)
+        })
+    })
 }
 
 module.exports = build
 
 if (require.main === module) {
-  start(minimist(process.argv.slice(2), {
-    integer: 'port',
-    alias: {
-      'port': 'p'
-    }
-  }))
+    start(minimist(process.argv.slice(2), {
+        integer: 'port',
+        alias: {
+            'port': 'p'
+        }
+    }))
 }
